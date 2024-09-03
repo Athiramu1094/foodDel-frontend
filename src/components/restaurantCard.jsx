8d027dd4f4741680929fff4b38c44599e7c550d8
Athira MU <krishna6athi@gmail.com>
2024-09-03 10:59:44 +0530

    updated

--- src/components/restaurantCard.jsx
+++ src/components/restaurantCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import './RestaurantCard.css'; 
+import './restaurantCard.css'; 
 
 const RestaurantCard = ({restaurant}) => {
     
